815ce7aff0e49a3ee1f46a6d5adccfecbd181c4b
exTerEX <developer@sagen.io>
2021-01-17 21:27:08 +0100

    Added TKinter implementation

---
+++ sudoku/game.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# pyright: reportMissingImports=false
+from board import Board
+
+
+class Game:
+    def __init__(self, board_file):
+        self.board_file = board_file
+        self.start_puzzle = Board(board_file).board
+
+    def start(self):
+        self.game_over = False
+        self.puzzle = []
+
+        for index in range(9):
+            self.puzzle.append([])
+
+            for jndex in range(9):
+                self.puzzle[index].append(self.start_puzzle[index][jndex])
+
+    def check_win(self):
+        for row in range(9):
+            if not self._check_row(row):
+                return False
+        for column in range(9):
+            if not self._check_column(column):
+                return False
+        for row in range(3):
+            for column in range(3):
+                if not self._check_square(row, column):
+                    return False
+        self.game_over = True
+        return True
+
+    def _check_block(self, block):
+        return set(block) == set(range(1, 10))
+
+    def _check_row(self, row):
+        return self._check_block(self.puzzle[row])
+
+    def _check_column(self, column):
+        return self._check_block(
+            [self.puzzle[row][column] for row in range(9)]
+        )
+
+    def _check_square(self, row, column):
+        return self._check_block(
+            [
+                self.puzzle[r][c]
+                for r in range(row * 3, (row + 1) * 3)
+                for c in range(column * 3, (column + 1) * 3)
+            ]
+        )
---
+++ sudoku/main.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+from tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
+
+# pyright: reportMissingImports=false
+from game import Game
+
+BOARDS = ["easy", "hard", "debug", "error"]
+MARGIN = 20
+SIDE = 50
+WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9
+GRID_LEN = 9
+
+
+class Interface(Frame):
+    def __init__(self, parent, obj):
+        self.game = obj
+        Frame.__init__(self, parent)
+        self.parent = parent
+
+        self.row, self.col = -1, -1
+
+        self.init()
+
+    def init(self):
+        self.parent.title("Sudoku")
+        self.pack(fill=BOTH)
+        self.canvas = Canvas(self, width=WIDTH, height=HEIGHT)
+        self.canvas.pack(fill=BOTH, side=TOP)
+        clear_button = Button(self, text="Clear answers", command=self._clear_answers)
+        clear_button.pack(fill=BOTH, side=BOTTOM)
+
+        self._draw_grid()
+        self._draw_puzzle()
+
+        self.canvas.bind("<Button-1>", self._cell_clicked)
+        self.canvas.bind("<Key>", self._key_pressed)
+
+    def _draw_grid(self):
+        for index in range(GRID_LEN + 1):
+            color = "black" if index % 3 == 0 else "gray"
+
+            # Vertical
+            self.canvas.create_line(
+                MARGIN + index * SIDE,
+                MARGIN,
+                MARGIN + index * SIDE,
+                HEIGHT - MARGIN,
+                fill=color)
+
+            # Horizontal
+            self.canvas.create_line(
+                MARGIN,
+                MARGIN + index * SIDE,
+                WIDTH - MARGIN,
+                MARGIN + index * SIDE,
+                fill=color)
+
+    def _draw_puzzle(self):
+        self.canvas.delete("numbers")
+
+        for index in range(GRID_LEN):
+            for jndex in range(GRID_LEN):
+                answer = self.game.puzzle[index][jndex]
+
+                if answer != 0:
+                    x_pos = MARGIN + jndex * SIDE + SIDE / 2
+                    y_pos = MARGIN + index * SIDE + SIDE / 2
+
+                    original = self.game.start_puzzle[index][jndex]
+                    color = "black" if answer == original else "sea green"
+
+                    self.canvas.create_text(x_pos, y_pos, text=answer, tags="numbers", fill=color)
+
+    def _draw_cursor(self):
+        self.canvas.delete("cursor")
+
+        if self.row >= 0 and self.col >= 0:
+            x_0 = MARGIN + self.col * SIDE + 1
+            y_0 = MARGIN + self.row * SIDE + 1
+            x_1 = MARGIN + (self.col + 1) * SIDE - 1
+            y_1 = MARGIN + (self.row + 1) * SIDE - 1
+
+            self.canvas.create_rectangle(x_0, y_0, x_1, y_1, outline="red", tags="cursor")
+
+    def _draw_victory(self):
+        x_0 = y_0 = MARGIN + SIDE * 2
+        x_1 = y_1 = MARGIN + SIDE * 7
+
+        self.canvas.create_oval(x_0, y_0, x_1, y_1, tags="victory",
+                                fill="dark orange", outline="orange")
+
+        x_pos = y_pos = MARGIN + 4 * SIDE + SIDE / 2
+
+        self.canvas.create_text(
+            x_pos,
+            y_pos,
+            text="You win!",
+            tags="victory",
+            fill="white",
+            font=(
+                "Arial",
+                32))
+
+    def _cell_clicked(self, event):
+        if self.game.game_over:
+            return
+
+        x_pos, y_pos = event.x, event.y
+        if (MARGIN < x_pos < WIDTH - MARGIN and MARGIN < y_pos < HEIGHT - MARGIN):
+            self.canvas.focus_set()
+
+            row, col = int((y_pos - MARGIN) / SIDE), int((x_pos - MARGIN) / SIDE)
+
+            if (row, col) == (self.row, self.col):
+                self.row, self.col = -1, -1
+            elif self.game.puzzle[row][col] == 0:
+                self.row, self.col = row, col
+        else:
+            self.row, self.col = -1, -1
+
+        self._draw_cursor()
+
+    def _key_pressed(self, event):
+        if self.game.game_over:
+            return
+
+        if self.row >= 0 and self.col >= 0 and event.char in "1234567890":
+            self.game.puzzle[self.row][self.col] = int(event.char)
+            self.col, self.row = -1, -1
+
+            self._draw_puzzle()
+            self._draw_cursor()
+
+            if self.game.check_win():
+                self._draw_victory()
+
+    def _clear_answers(self):
+        self.game.start()
+        self.canvas.delete("victory")
+        self._draw_puzzle()
+
+
+if __name__ == "__main__":
+    NAME = "easy"
+
+    with open(f"sudoku/resources/deck/{NAME}.sudoku", "r") as file:
+        game = Game(file)
+        game.start()
+
+        root = Tk()
+
+        root.wm_iconbitmap("sudoku/resources/logo/sudoku-icon.ico")
+
+        Interface(root, game)
+
+        root.geometry(f"{WIDTH}x{HEIGHT + 40}")
+        root.mainloop()
